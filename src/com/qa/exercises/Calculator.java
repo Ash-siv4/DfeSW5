df41a9299c051308561c3c6c36e4be2c4125fcde
Aswene Sivaraj <ashsiva4@hotmail.co.uk>
2021-11-08 17:04:03 +0000

    AS: calculator exercise

--- src/com/qa/exercises/Calculator.java
+++ src/com/qa/exercises/Calculator.java
@@ -2,4 +2,38 @@ package com.qa.exercises;
 
 public class Calculator {
 
+//	Create a method that accepts two integers as input, then returns an integer that is the sum of these two parameters. (Addition)
+//
+//	Create the following additional three methods that each take two parameters:
+//
+//	Multiplication - which takes two numbers and returns the product.
+//	Subtraction - which takes two numbers, then returns the result of the subtraction.
+//	Division - which takes two numbers, then returns the result of the division.
+//	Your division method may have returned the wrong result; 
+//	This is called a rounding error and is quite common in most languages. 
+//	This is because we were using ints rather than doubles. 
+//	Modify the division method so that it takes Double parameters and then return a Double, if not already.
+	
+	
+	public static int add(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	public static int multiply(int num1, int num2) {
+		return num1 * num2;
+	}
+	
+	public static int sub(int num1, int num2) {
+		return num1 - num2;
+	}
+	
+	public static int divideInt(int num1, int num2) {
+		return num1 / num2;
+	}
+	
+	public static double divide(double num1, double num2) {
+		return num1 / num2;
+	}
+	
+	
 }
--- src/com/qa/exercises/Runner.java
+++ src/com/qa/exercises/Runner.java
@@ -4,8 +4,15 @@ public class Runner {
 
 	public static void main(String[] args) {
 		
+		System.out.println(Calculator.add(12, 43));
 		
+		System.out.println(Calculator.multiply(12, 43));
 		
+		System.out.println(Calculator.sub(12, 43));
+		
+		System.out.println(Calculator.divideInt(5, 2));
+		
+		System.out.println(Calculator.divide(5, 2));
 	}
 
 }
